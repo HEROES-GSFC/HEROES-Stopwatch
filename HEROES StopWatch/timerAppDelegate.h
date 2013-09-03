ff23cef2fc39cda1edd7721b5c5472ec7c9c2bfc
Steven Christe <steven.d.christe@nasa.gov>
2013-09-02 22:31:44 -0400

    Initial Commit

---
+++ HEROES StopWatch/timerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  timerAppDelegate.h
+//  HEROES StopWatch
+//
+//  Created by Steven Christe on 9/2/13.
+//  Copyright (c) 2013 Steven Christe NASA GSFC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface timerAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
